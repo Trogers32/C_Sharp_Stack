075c925e667976814edb59b0c9ba94a498f38fa5
Tim <timrogers32@gmail.com>
2019-09-30 13:17:24 -0700

    MultipTable finished

---
+++ Language Essentials/MultipTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultipTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[10,10];
+            for(int i = 1; i< 11;i++){
+                for(var mul = 1; mul<11;mul++){
+                    arr[i-1,mul-1] = mul*i;
+                }
+            }
+            for(int i =0;i<10;i++){
+                    string arr2 = "";
+                for(int j =0;j<10;j++){
+                    arr2 += " " + arr[i,j];
+                }
+                Console.WriteLine(arr2);
+            }
+        }
+    }
+}
