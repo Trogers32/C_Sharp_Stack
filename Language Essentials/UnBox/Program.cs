498a76b65f8d619135167d7ebbe0c31d1d6e24d7
Tim <timrogers32@gmail.com>
2019-09-30 13:29:11 -0700

    Boxing/Unboxing

---
+++ Language Essentials/UnBox/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> objs = new List<object>();
+            objs.Add(7);
+            objs.Add(28);
+            objs.Add(-1);
+            objs.Add(true);
+            objs.Add("chair");
+            int sum = 0;
+            foreach(var i in objs){
+                Console.WriteLine(i);
+                if(i is int){
+                    sum += (int)i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+//  Create an empty List of type object
+//  Add the following values to the list: 7, 28, -1, true, "chair"
+//  Loop through the list and print all values (Hint: Type Inference might help here!)
+//  Add all values that are Int type together and output the sum
