00d690a826b5f3b4c833261e3f799c3b3e1418ad
Tim <timrogers32@gmail.com>
2019-10-04 12:25:29 -0700

    DSValid Finished

---
+++ ASP_NET Core/MVC2/DSValid/Models/User.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DSValid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+    public class User
+    {
+///////////////////////////////////////////////////////////////////////
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(2, ErrorMessage = "Name must be 2 or more")]
+        public string Username {get;set;}
+///////////////////////////////////////////////////////////////////////
+        [Required(ErrorMessage = "Location is required")]
+        public string Location { get; set; }
+///////////////////////////////////////////////////////////////////////
+        [Required(ErrorMessage = "Favorite Language is required")]
+        public string Language {get;set;}
+///////////////////////////////////////////////////////////////////////
+        [Range(0,20, ErrorMessage = "Comment must be less than 20")]
+        public string Comment {get;set;}
+///////////////////////////////////////////////////////////////////////
+    }
+}
